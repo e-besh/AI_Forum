bc8a2189086091981edaaa5946c780f8f873facc
e-besh <enasbeshir2025@gmail.com>
2026-09-20 23:22:15 +0300

    add a comment

--- backend/src/api/rag/rag.route.js
+++ backend/src/api/rag/rag.route.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { authenticateUser } from "../../middleware/auth.middleware";
 
+// * post /api/mysql
+// * get /api/mysql
 const router = express.Router();
 
 router.post("/", authenticateUser);
